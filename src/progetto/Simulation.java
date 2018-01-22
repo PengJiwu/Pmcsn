33d32319c0eae430e39bccb9ed1b879edc17c8c3
simone <simone.zippo@gmail.com>
2018-01-22 13:14:07 +0100

    cloudlet

---
+++ src/progetto/Simulation.java
@@ -0,0 +1,46 @@
+package progetto;
+
+
+
+/*      TODO LIST:
+*
+*       Lista di eventi <- Interfaccia/classe astrata evento -> ArrivoCloudlet - ArrivoCloud - ComplCloudlet - ComplCloud
+*       Clock che scorre in Simulation
+*       Gestione degli eventi
+*
+*
+* */
+
+public class Simulation {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+
+
+    private void run()
+    {
+        /*
+        //1) Genera arrivo
+
+        //While(c'è un evento)
+
+            pop evento dalla event list
+            gestisci evento
+            NB gestisci evento probabile che genera nuovi eventi (un arrivo genera un evento di completamento, un arrivo può generare un arrivo in cloud)
+            aggiorna eventlist
+            stima dei parametri per classe/per coda/ globali/ ecc
+
+            scorri il clock
+
+
+
+
+        */
+    }
+}
