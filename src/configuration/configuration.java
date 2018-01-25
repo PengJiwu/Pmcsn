6911bce1998de15384ec16485f4c888b6367ec8e
Marius Dragos Ionita <ionita.maryus@gmail.com>
2018-01-25 10:02:41 +0100

    Add configuration file Add createFile with statisticsse ..

---
+++ src/configuration/configuration.java
@@ -0,0 +1,25 @@
+package configuration;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class configuration {
+
+    public static final String FILENAME = "../Pmcsn/src/configuration/configuration";
+    public static int N = 0;
+    public static int S = 0;
+    public static int seed = 0;
+    public static double duration = 0;
+
+    public static void loadFIle() throws Exception {
+
+        Properties parameters = new Properties();
+        FileInputStream in = new FileInputStream(FILENAME);
+        parameters.load(in);
+        N = Integer.parseInt(parameters.getProperty("N"));
+        S = Integer.parseInt(parameters.getProperty("S"));
+        seed = Integer.parseInt(parameters.getProperty("seed"));
+        duration = Double.parseDouble(parameters.getProperty("duration"));
+
+    }
+}
